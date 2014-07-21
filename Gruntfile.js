f724fbf6d9902c489d14a7fbcfe9f62aebf6f0ce
Welch Canavan <welch.canavan@gmail.com>
2014-07-21 13:53:15 -0400

    add images in subfolders to grunt deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -281,6 +281,7 @@ module.exports = function (grunt) {
             // Usemin moves CSS and javascript inside of Usemin blocks.
             // Copy moves asset files and directories.
             'img/**/*',
+            '**/*.jpg',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}'
