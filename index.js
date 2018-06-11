9bcd9c26ca0772973d703ef13341936f55de0a77
Welch Canavan <welch.canavan@gmail.com>
2018-06-11 13:47:24 -0400

    add test script

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
-const browserSync = require('metalsmith-browser-sync');
-const collections = require('metalsmith-collections');
-const htmlMinifier = require('metalsmith-html-minifier');
-const inPlace = require('metalsmith-in-place');
-const layouts = require('metalsmith-layouts');
-const markdown = require('metalsmith-markdown');
-const metallic = require('metalsmith-metallic');
-const Metalsmith = require('metalsmith');
-const msIf = require('metalsmith-if');
-const permalinks = require('metalsmith-permalinks');
-const watch = require('metalsmith-watch');
+const browserSync = require('metalsmith-browser-sync')
+const collections = require('metalsmith-collections')
+const htmlMinifier = require('metalsmith-html-minifier')
+const inPlace = require('metalsmith-in-place')
+const layouts = require('metalsmith-layouts')
+const markdown = require('metalsmith-markdown')
+const metallic = require('metalsmith-metallic')
+const Metalsmith = require('metalsmith')
+const msIf = require('metalsmith-if')
+const permalinks = require('metalsmith-permalinks')
+const watch = require('metalsmith-watch')
 
-const isDevBuild = process.argv[2] === 'dev';
-const isProdBuild = process.argv[2] === 'prod';
+const isDevBuild = process.argv[2] === 'dev'
+const isProdBuild = process.argv[2] === 'prod'
 
 Metalsmith(__dirname)
-    .metadata({
-        siteName: 'Welch Canavan: UX Developer',
-        siteURL: 'http://welchcanavan.com/',
-        GA: 'UA-41030734-1',
+  .metadata({
+    siteName: 'Welch Canavan: UX Developer',
+    siteURL: 'http://welchcanavan.com/',
+    GA: 'UA-41030734-1'
+  })
+  .source('./src/site')
+  .use(collections({
+    posts: {
+      pattern: 'posts/**/*.md',
+      sortBy: 'date',
+      reverse: true
+    },
+    works: 'work/**/*.md'
+  }))
+  .use(metallic())
+  .use(markdown())
+  .use(layouts({
+    default: 'default.njk',
+    engine: 'nunjucks'
+  }))
+  .use(inPlace())
+  .use(permalinks({
+    linksets: [
+      {
+        match: { collection: 'posts' },
+        pattern: ':slug'
+      },
+      {
+        match: { collection: 'works' },
+        pattern: ':slug'
+      }
+    ]
+  }))
+
+  // development specific tasks
+  .use(msIf(
+    isDevBuild,
+    watch({
+      paths: {
+        './src/site/**/*': true,
+        './layouts/**/*': '**/*'
+      }
     })
-    .source('./src/site')
-    .use(collections({
-        posts: {
-            pattern: 'posts/**/*.md',
-            sortBy: 'date',
-            reverse: true
-        },
-        works: 'work/**/*.md'
-    }))
-    .use(metallic())
-    .use(markdown())
-    .use(layouts({
-        default: 'default.njk',
-        engine: 'nunjucks'
-    }))
-    .use(inPlace())
-    .use(permalinks({
-        linksets: [
-            {
-                match: { collection: 'posts' },
-                pattern: ':slug'
-            },
-            {
-                match: { collection: 'works' },
-                pattern: ':slug'
-            }
-        ]
-    }))
+  ))
+  .use(msIf(
+    isDevBuild,
+    browserSync()
+  ))
 
-    // development specific tasks
-    .use(msIf(
-        isDevBuild,
-        watch({
-            paths: {
-                "${source}/**/*": true,
-                "layouts/**/*": "**/*",
-            },
-        })
-    ))
-    .use(msIf(
-        isDevBuild,
-        browserSync()
-    ))
-
-    // production specific tasks
-    .use(msIf(
-        isProdBuild,
-        htmlMinifier()
-    ))
-    .build(function (err) {
-        if (err) throw err;
-    });
+  // production specific tasks
+  .use(msIf(
+    isProdBuild,
+    htmlMinifier()
+  ))
+  .build(function (err) {
+    if (err) throw err
+  })
