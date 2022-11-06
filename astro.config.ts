a56ec7c3b96f899f9afe886bd3bca100c6c3641e
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2022-11-06 15:22:02 -0800

    fix build

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ site: "https://xiwcx.github.io" });
+export default defineConfig({
+  site: "https://xiwcx.github.io",
+  vite: { ssr: { noExternal: ["@mobily/ts-belt"] } },
+});
